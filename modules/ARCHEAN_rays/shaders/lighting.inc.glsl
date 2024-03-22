d749bf7d613be00cc5a440511e0b9db3fe042dab
olivier st-laurent <kzaurckichz@gmail.com>
2024-03-22 16:12:22 -0400

    ...

--- modules/ARCHEAN_rays/shaders/lighting.inc.glsl
+++ modules/ARCHEAN_rays/shaders/lighting.inc.glsl
@@ -79,7 +79,7 @@ float caustics(vec3 worldPosition, vec3 normal, float t) {
 }
 
 vec3 GetDirectLighting(in vec3 worldPosition, in vec3 rayDirection, in vec3 normal, in vec3 albedo, in float referenceDistance, in float metallic, in float roughness, in float specular, in float specularHardness) {
-	vec3 position = worldPosition + normal * referenceDistance * 0.0001;
+	vec3 position = worldPosition + normal * referenceDistance * 0.001;
 	vec3 directLighting = vec3(0);
 	
 	rayQueryEXT q;
