703c592650b7216844bd2532ae3057bc415ac720
olivier st-laurent <kzaurckichz@gmail.com>
2024-05-11 22:33:58 -0400

    release

--- xenon/renderer/shaders/histogram.comp.glsl
+++ xenon/renderer/shaders/histogram.comp.glsl
@@ -2,11 +2,10 @@
 
 void main() {
 	uvec2 size = imageSize(img_thumbnail);
-	uvec2 imageOffset = size/4;
 	vec4 luminance = vec4(0);
-	for (uint x = 0; x < size.s/2; x+=2) {
-		for (uint y = 0; y < size.t/2; y+=2) {
-			vec4 color = imageLoad(img_thumbnail, ivec2(imageOffset + uvec2(x,y)));
+	for (uint x = 0; x < size.s; x+=2) {
+		for (uint y = 0; y < size.t; y+=2) {
+			vec4 color = imageLoad(img_thumbnail, ivec2(x,y));
 			luminance += vec4(clamp(color.rgb, vec3(0.01), vec3(1e3)), 0.25);
 		}
 	}
