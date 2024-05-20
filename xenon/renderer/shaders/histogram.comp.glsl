40831288fe285bbef29b532243197fc3c8e63fa7
olivier st-laurent <kzaurckichz@gmail.com>
2024-05-20 15:52:47 -0400

    [list] [*] Added FluidValve component [*] Added AngularVelocitySensor [*] Improved histogram / eye adaptation algo to prevent sudden exposure variation when looking at some bright things like the Sun or SpotLights at certain angles [*] Improved network versioning system [*] Added Archean-data/server/mods.txt, deprecated Archean-data/mods.txt [*] Fixed Thrusters plasma exposure [*] Bugfix Adventure Mode Blueprint Spawn requiring impossible to obtain 'Dashboard' component. Now only requires to have a DashboardTool but won't consume it [*] Bugfix Adventure Mode Blueprint Spawn requiring impossible to obtain 'Build' component [*] Bugfix Adventure Mode Blueprint Spawn failing with brand new cable spools [/list]

--- xenon/renderer/shaders/histogram.comp.glsl
+++ xenon/renderer/shaders/histogram.comp.glsl
@@ -3,10 +3,10 @@
 void main() {
 	uvec2 size = imageSize(img_thumbnail);
 	vec4 luminance = vec4(0);
-	for (uint x = 0; x < size.s; x+=2) {
-		for (uint y = 0; y < size.t; y+=2) {
+	for (uint x = 0; x < size.s; x++) {
+		for (uint y = 0; y < size.t; y++) {
 			vec4 color = imageLoad(img_thumbnail, ivec2(x,y));
-			luminance += vec4(clamp(color.rgb, vec3(0.01), vec3(1e3)), 0.25);
+			luminance += vec4(clamp(color.rgb, vec3(0.01), vec3(100)), 0.25);
 		}
 	}
 	xenonRendererData.histogram_total_luminance.r = luminance.r;
