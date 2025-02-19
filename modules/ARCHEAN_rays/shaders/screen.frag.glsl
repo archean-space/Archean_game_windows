58663fdfad1232d0dc32c6c6bd1c3cd0fa126d58
Olivier St-Laurent <olivier@xenon3d.com>
2025-02-19 00:10:39 -0800

    Published new release

--- modules/ARCHEAN_rays/shaders/screen.frag.glsl
+++ modules/ARCHEAN_rays/shaders/screen.frag.glsl
@@ -13,17 +13,16 @@ float GetTrueDistanceFromDepthBuffer(float depth) {
 	return 2.0 * (xenonRendererData.config.zFar * xenonRendererData.config.zNear) / (xenonRendererData.config.zNear + xenonRendererData.config.zFar - (depth * 2.0 - 1.0) * (xenonRendererData.config.zNear - xenonRendererData.config.zFar));
 }
 
-const float maxScreenDistance = 10.0;
+const float maxScreenDistance = 25.0;
 
 void main() {
-	// depth test
-	float screenDepth = in_position.z * 1.03 / in_position.w;
-	float screenDistance = GetTrueDistanceFromDepthBuffer(screenDepth);
+	float screenDistance = GetTrueDistanceFromDepthBuffer(in_position.z / in_position.w);
 	if (screenDistance > maxScreenDistance) discard;
 	vec2 uv = gl_FragCoord.xy / imageSize(img_emission);
-	float solidDepth = texture(sampler_depth, uv).r;
-	if (screenDepth < solidDepth) discard;
+	vec4 solidDepth = textureGather(sampler_depth, uv, 0);
+	float solidDistance = GetTrueDistanceFromDepthBuffer((solidDepth[0] + solidDepth[1] + solidDepth[2] + solidDepth[3]) / 4) + 0.02;
+	if (screenDistance > solidDistance) discard;
 	vec4 color = texture(textures[screen.monitorIndex], in_uv) * xenonRendererData.config.globalLightingFactor;
-	out_post = vec4(color.rgb, color.a * smoothstep(maxScreenDistance, maxScreenDistance/2, GetTrueDistanceFromDepthBuffer(screenDepth)));
-	// out_post = vec4(in_uv, 0, 1);
+	out_post = vec4(color.rgb, color.a * smoothstep(maxScreenDistance, maxScreenDistance/2, screenDistance));
+	// out_post = vec4(in_uv, 0, 1); // debug UVs
 }
